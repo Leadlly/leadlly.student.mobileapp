5380296120f81f5db15bfc15c47e503ed7b00b55
adil-khursheed <adilkhursheed60@gmail.com>
2024-12-11 19:47:18 +0530

    fix: fixed subscription valid date and control panel

--- app/(subscription)/subscription-plans.tsx
+++ app/(subscription)/subscription-plans.tsx
@@ -56,7 +56,7 @@ const SubscriptionPlansScreen: React.FC = () => {
   const userCategory = user?.category || "free"; // Assume "free" if user category is null or undefined
 
   // Filter the plans based on userCategory
-  const filteredPlans = pricingData?.pricing
+  const filteredPlans = pricingData?.pricing;
 
   const mergedPricingData = filteredPlans?.map((pricing) => {
     const matchingDetails = subscriptionDetails.find(
@@ -258,55 +258,97 @@ const SubscriptionPlansScreen: React.FC = () => {
                         ? "bg-[#FFD9AE]"
                         : plan?.title === "consistency"
                           ? "bg-[#D8BDFF]"
-                          : "bg-[#C8FFB6]"
+                          : "bg-[#C8FFB6]",
+                      user &&
+                        user.subscription.status === "active" &&
+                        user?.subscription.planId === plan?.planId
+                        ? "border-2 border-primary"
+                        : ""
                     )}
                   >
                     <View className="border-b border-tab-item-gray py-2 flex-row items-center justify-between">
-                      <Text className="capitalize text-lg font-mada-Bold leading-6">
+                      <Text className="flex-1 capitalize text-lg font-mada-Bold leading-6">
                         {plan?.title} Plan
                       </Text>
-                      {plan?.title === "consistency" && (
-                        <LinearGradient
-                          colors={[
-                            "rgba(248, 155, 5, 1)",
-                            "rgba(180, 56, 243, 1)",
-                          ]}
-                          dither={false}
-                          start={{ x: 0.1, y: 0 }}
-                          end={{ x: 1, y: 1 }}
-                          className="items-center justify-center py-1 px-3 rounded-full"
-                        >
-                          <Text className="text-[10px] font-mada-Bold leading-4 text-white">
-                            Most Popular
-                          </Text>
-                        </LinearGradient>
-                      )}
+                      <View className="flex-row items-center space-x-2">
+                        {plan?.title === "consistency" && (
+                          <LinearGradient
+                            colors={[
+                              "rgba(248, 155, 5, 1)",
+                              "rgba(180, 56, 243, 1)",
+                            ]}
+                            dither={false}
+                            start={{ x: 0.1, y: 0 }}
+                            end={{ x: 1, y: 1 }}
+                            className="items-center justify-center py-1 px-3 rounded-full"
+                          >
+                            <Text className="text-[10px] font-mada-Bold leading-4 text-white">
+                              Most Popular
+                            </Text>
+                          </LinearGradient>
+                        )}
+
+                        {user &&
+                        user.subscription.status === "active" &&
+                        user.subscription.planId === plan?.planId ? (
+                          <View className="bg-primary px-3 py-1 rounded-full">
+                            <Text className="text-xs font-mada-medium text-white">
+                              Active Plan
+                            </Text>
+                          </View>
+                        ) : null}
+                      </View>
                     </View>
 
                     <View className="flex-row items-center space-x-1 pt-3">
                       <Text className="text-3xl leading-8 font-mada-Bold">
-                        ₹{Math.round(Number(plan?.amount) / Number(plan?.["duration(months)"]))} 
+                        ₹
+                        {Math.round(
+                          Number(plan?.amount) /
+                            Number(plan?.["duration(months)"])
+                        )}
                       </Text>
                       <Text className="text-base font-mada-semibold">
-                        /months
+                        / month
                       </Text>
                     </View>
 
                     <View className="flex-row items-center space-x-1">
-                      <Text className="text-xs font-mada-regular">{plan?.discountPercentage}% OFF</Text>
+                      <Text className="text-xs font-mada-regular">
+                        {plan?.discountPercentage}% OFF
+                      </Text>
                       <View className="relative">
-                        <View className="absolute top-1/2 left-0 -translate-y-0.5 -rotate-12 w-9 h-0.5 bg-leadlly-red" />
+                        <View className="absolute top-1/2 left-0 -translate-y-0.5 -rotate-[6deg] w-16 h-0.5 bg-leadlly-red" />
                         <Text className="text-xs font-mada-regular">
-                          ₹{Math.round(Number(plan?.initialPrice) / Number(plan?.["duration(months)"]))}/month
+                          ₹
+                          {Math.round(
+                            Number(plan?.initialPrice) /
+                              Number(plan?.["duration(months)"])
+                          )}
+                          / month
                         </Text>
                       </View>
                     </View>
                     <Text className="text-xs font-mada-regular font-bold py-1">
-                      Valid till: {new Date(new Date().setMonth(new Date().getMonth() + Number(plan?.["duration(months)"])))
-                        .toLocaleDateString('en-GB')}
-                      {plan?.title === "momentum" && " (1 month)"}
-                      {plan?.title === "consistency" && " (JEE/NEET 2025)"}
-                      {plan?.title === "sturdy-step" && " (JEE/NEET 2026)"}
+                      Valid till:{" "}
+                      {user?.subscription.status === "active"
+                        ? new Date(
+                            new Date().setMonth(
+                              new Date(
+                                user.subscription.dateOfDeactivation || ""
+                              ).getMonth() + Number(plan?.["duration(months)"])
+                            )
+                          ).toLocaleDateString("en-GB")
+                        : `${plan?.["duration(months)"]} month${Number(plan?.["duration(months)"]) > 1 ? "s" : ""}`}
+                      {user?.subscription.status === "active" &&
+                        plan?.title === "momentum" &&
+                        " (1 month)"}
+                      {user?.subscription.status === "active" &&
+                        plan?.title === "consistency" &&
+                        " (JEE/NEET 2025)"}
+                      {user?.subscription.status === "active" &&
+                        plan?.title === "sturdy-step" &&
+                        " (JEE/NEET 2026)"}
                     </Text>
 
                     <Link
